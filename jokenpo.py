ae29ef7674e45496525294bcc4f1db0a51ff5ac3
Cláudio Aguiar <claudioaguiargomes@gmail.com>
2022-07-18 13:11:35 -0400

    Update jokenpo.py
    Melhorias no código

--- jokenpo.py
+++ jokenpo.py
@@ -1,3 +1,4 @@
+from ast import Try
 import random # --> Elementos randomicos
 from time import sleep # --> Elementos de espera
 r = 1  # --> Resposta de continuação
@@ -16,15 +17,18 @@ print('Bem-vindo ao Jokenpô, eu sou o Charlie :D')
 print('-=-'*20)
 
 sleep(2)
-jogador = input('Como posso te chamar?') # --> Nome do jogador
+jogador = input('Como posso te chamar? ') # --> Nome do jogador
 sleep(1)
 print('Charlie: Olá {} Conforme formos jogando, sua pontuação aparecerá em \033[1;32mVERDE'.format(jogador))
 sleep(1)
 # -------------- Fim do cabeçalho -------------- #
-print('\033[1;33mCharlie: Vamos comçar?')
+print('\033[1;33mCharlie: Vamos começar?')
 
-r = int(input('[ 1 ] - SIM\n[ 2 ] - Não'))
-
+try:
+    r = int(input('[ 1 ] - SIM\n[ 2 ] - Não\n'))
+except:
+    r = 3
+sleep(1)
 if r == 1:
     print('\033[1;32mPontuação do jogador {}: {}'.format(jogador,pj))
     print('Pontuação do Charlie: {}'.format(pm))
@@ -32,8 +36,10 @@ elif r == 2:
     print('\33[mPoxa, que pena, foi um prazer te conhecer!')
     exit()
 else:
-    print('Você digitou um número diferente, irei considerar que quer continuar!!')
-
+    print('Você digitou algo diferente, irei considerar que quer continuar!!')
+sleep(1)
+print('\033[1;37mIniciando jogo...\033[m\n')
+sleep(2)
 # -------------- Inicio da rodada -------------- #
 while i < ro:
     i = i+1
@@ -47,27 +53,28 @@ while i < ro:
     sleep(1)
 
     if v == e:
-        print('Charlie: Ninguém ganhou!!')
+        print('Charlie: Nós escolhemos a mesma opção. Ninguém ganhou!!'.format(v))
     elif v == 1 and e == 2:
-        print('Charlie: Escolhi Papel, Eu Ganhei!! haha')
+        print('Charlie: Escolhi \033[34mPapel,\033[m e você escolheu \033[34mPedra.\033[1;31m Você perdeu!! haha\033[m')
         pm = pm + 1
     elif v == 1 and e == 3:
-        print('Charlie: Você Ganhou! :(')
+        print('Charlie: Eu escolhi \033[34mTesoura, \033[mVocê escolheu \033[34mPedra. \033[1;35mVocê Ganhou! :(\033[m')
         pj = pj + 1
     elif v == 2 and e == 1:
-        print('Charlie: Você Ganhou! :(')
+        print('Charlie: Eu escolhi \033[34mPedra, \033[mVocê escolheu \033[34mPapel. \033[1;35mVocê Ganhou! :(\033[m')
         pj = pj + 1
     elif v == 3 and e == 1:
-        print('Charlie: Escolhi pedra, Eu Ganhei!! haha')
+        print('Charlie: Escolhi \033[34mPedra,\033[m e você escolheu \033[34mTesoura.\033[1;31m Você perdeu!! haha\033[m')
         pm = pm + 1
     elif v == 3 and e == 2:
-        print('Charlie: Você Ganhou! :(')
+        print('Charlie: Eu escolhi \033[34mPapel, \033[mVocê escolheu \033[34mTesoura. \033[1;35m Você Ganhou! :(\033[m')
         pj = pj + 1
 
     elif v == 4:
         print('\33[mCharlie: Poxa, que pena, foi um prazer te conhecer!')
         exit()
-
+    sleep(1)
     print('\033[1;32mPontuação do jogador {}: {}'.format(jogador, pj))
     print('Pontuação do Charlie: {}'.format(pm))
+    sleep(1)
     # -------------- Fim da rodada -------------- #
